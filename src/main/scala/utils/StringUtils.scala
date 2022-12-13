da9964a4fb523401265687981fc5e191ced6cff9
Jon Jakova <jon.jakova@sabanet.it>
2022-12-13 17:26:14 +0100

    fix: string utils

--- src/main/scala/utils/StringUtils.scala
+++ src/main/scala/utils/StringUtils.scala
@@ -1,12 +1,14 @@
 package utils.StringUtils
 
 def is_blank(str: String): Boolean =
-  var i = -1
-  def check(char: String): Boolean = char == " "
-  i = i+1
-  if is_empty(str) then true else check( str.substring(i) )
+  is_empty(str) match
+    case true => true
+    case false => is_whitespace(str)
 
 def is_empty(s: String): Boolean =
   s match
     case "" => true
     case _ => false
+
+def is_whitespace(str: String): Boolean =
+  str.filter(_.isWhitespace).length == str.length
