eddf5bbb6640de4894768083f0f6691425ea2100
Jon Jakova <jon.jakova@sabanet.it>
2022-12-14 10:37:06 +0100

    feat: add exclusive option

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,15 +3,18 @@ import utils.StringUtils
 import utils.GeneralUtils
 
 @main def main(file_path: String, query: String, options: String*): Unit =
-  val ignore_case = detach_args(options)
+  val (ignore_case, exclusive) = detach_args(options)
   Source.fromFile(file_path)
     .getLines()
     .filter(line => !StringUtils.is_blank(line))
-    .filter(line => line_contains(line, query, ignore_case))
+    .filter(line => if exclusive then !line_contains(line, query, ignore_case) else line_contains(line, query, ignore_case))
     .foreach(println)
 
 def line_contains(line: String, query: String, ignore_case: Boolean): Boolean =
   if ignore_case then line.toLowerCase.contains(query.toLowerCase)
   else line.contains(query)
 
-def detach_args(args: Seq[String]): Boolean = GeneralUtils.is_some(args.find(arg => arg == "--ignore-case"))
+def detach_args(args: Seq[String]): (Boolean, Boolean) =
+  val cases = GeneralUtils.is_some(args.find(arg => arg == "--ignore-case"))
+  val exclusive = GeneralUtils.is_some(args.find(arg => arg == "--exclusive"))
+  (cases, exclusive)
