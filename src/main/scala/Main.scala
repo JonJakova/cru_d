25fd0fbb21f926ac2a0191776b449f97c304982f
Jon Jakova <jon.jakova@sabanet.it>
2022-12-14 10:45:36 +0100

    fix: memory leak

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,16 @@
 import scala.io.Source
 import utils.StringUtils
 import utils.GeneralUtils
+import scala.util.Using
 
 @main def main(file_path: String, query: String, options: String*): Unit =
   val (ignore_case, exclusive) = detach_args(options)
-  Source.fromFile(file_path)
-    .getLines()
-    .filter(line => !StringUtils.is_blank(line))
-    .filter(line => if exclusive then !line_contains(line, query, ignore_case) else line_contains(line, query, ignore_case))
-    .foreach(println)
+  Using(Source.fromFile(file_path)) { source =>
+    source.getLines()
+      .filter(line => !StringUtils.is_blank(line))
+      .filter(line => if exclusive then !line_contains(line, query, ignore_case) else line_contains(line, query, ignore_case))
+      .foreach(println)
+  }
 
 def line_contains(line: String, query: String, ignore_case: Boolean): Boolean =
   if ignore_case then line.toLowerCase.contains(query.toLowerCase)
